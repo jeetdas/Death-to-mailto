ec3b568b6a99147f55a525dc66a6e4c73da7e49d
Jeet Das <jeet@das.bz>
2019-03-07 13:34:57 -0600

    Added notifications using pnotify

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('merged-jquery-3.3.1.min-pnotify.custom.min.js');
+(document.head||document.documentElement).appendChild(s);
+
 const anchorTags = document.body.getElementsByTagName("a");
 
 Array.from(anchorTags).forEach(function (tag) {
@@ -5,6 +9,22 @@ Array.from(anchorTags).forEach(function (tag) {
         var regex = /\S+[a-z0-9]@[a-z0-9\.]+/img
         var emailAddress = tag.href.split('?')[0].split('&')[0].match(regex)[0];
         emailAddress = emailAddress.replace("mailto:", "");
-        tag.href = 'javascript:{navigator.clipboard.writeText(\"' + emailAddress + '\").then(() => { console.log(\'Successsfully destroyed mailto!\'); }).catch(err => { console.log(\'Something went wrong with mailto :(\', err); });}';
+        tag.href = `javascript:
+                        {
+                            navigator.clipboard.writeText("${emailAddress}")
+                                .then(() => {
+                                    new PNotify({
+                                        title: 'Copied',
+                                        text: 'Copied email address ${emailAddress}',
+                                        type: 'success',
+                                        delay: 2000
+                                    });
+                                })
+                                .catch(err => {
+                                    console.log("Something went wrong with mailto :(", err);
+                                }
+                            );
+                        }
+                    `;
     }
 });
